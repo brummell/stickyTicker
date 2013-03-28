a17d59e8f7915de05ee258e8ed6a319bf5780ba0
Douglas Brummell <doug.brummell@gmail.com>
2013-03-28 18:42:08 -0400

    Commit numero tres?

--- FrameTest.java
+++ FrameTest.java
@@ -1,6 +1,9 @@
 package ticker;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.*;
 import org.json.simple.*;
 
@@ -67,61 +70,89 @@ class SimpleFrame extends JFrame {
 		JScrollPane fieldsPane = new JScrollPane(fieldsList);
 		JScrollPane daysPane = new JScrollPane(daysList);
 
-		JCheckBox log = new JCheckBox("Log each transaction to text?"),
-				printToScreen = new JCheckBox("Print results to screeen?");
+		log = new JCheckBox("Log each transaction to text?");
+		printToScreen = new JCheckBox("Print results to screeen?");
 
 		buttonPane = new JPanel();
 		
-		JTextField freqField = new JTextField("Enter integer", 15),
-				symbolField = new JTextField("Enter symbols (X1+X2+...+X3)", 25),
-				hourBegField = new JTextField("Enter time (int.)", 10),
-				hourEndField = new JTextField("Enter time (int.)", 10);
+		symbolField = new JTextField("Enter symbols (X1+X2+...+X3)", 20);
+		freqField = new JTextField("Enter integer", 8);
+		hourBegField = new JTextField("Enter time (int.)", 10);
+		hourEndField = new JTextField("Enter time (int.)", 10);
+		fileNameField = new JTextField("Enter conf. name", 20);
 		
 		JLabel stocks = new JLabel("Symbols: "), options = new JLabel("Fields: "),
-				 run = new JLabel("Run every: "), days = new JLabel("On days: "),
-				 hourStart = new JLabel("Between: "), hourFinish = new JLabel("And: ");
+				run = new JLabel("Run every: "), days = new JLabel("On days: "),
+				hourStart = new JLabel("Between: "), hourFinish = new JLabel("And: "),
+				saveName = new JLabel("Save as: ");
 
 		JButton subButton = new JButton("Submit");
 		subButton.addActionListener( new ActionListener() {
+			@SuppressWarnings("unchecked")
 			public void actionPerformed(ActionEvent event) {
-				System.out.println("This happened");
-				String timeUnit = (String) hoursBox.getSelectedItem();
-				System.out.println(timeUnit);
-				
-				Object[] values = fieldsList.getSelectedValues();
-				for (Object value : values)
-					System.out.println((String) value);
-
+				List<String> fields = new ArrayList<String>();
+				List<String> days = new ArrayList<String>();
+				Object[] fieldsValues = fieldsList.getSelectedValues();
+				Object[] daysValues = daysList.getSelectedValues();
+	
+				for (Object value : fieldsValues) {
+					fields.add((String) value); 
+				}		
+				for (Object value : daysValues) {
+					days.add((String) value); 
 				}
-			});
+				
+				JSONObject obj = new JSONObject();
+				 	obj.put("symbolString",symbolField.getText());
+				 	obj.put("freq",freqField.getText());
+					obj.put("hourBeg",hourBegField.getText());
+					obj.put("hourEnd",hourEndField.getText());
+					obj.put("shouldLog", log.isSelected());
+					obj.put("shouldPrint", printToScreen.isSelected());
+					obj.put("unit", (String) hoursBox.getSelectedItem());
+					obj.put("saveName", fileNameField.getText());
+					obj.put("fields", fields);
+					obj.put("days", days);
+				
+				  System.out.print(obj);
+			}
+		});
 		
 		add(stocks, new GBC(0, 0).setAnchor(GBC.WEST).setWeight(100, 100));
 		add(options, new GBC(0, 1).setAnchor(GBC.WEST).setWeight(100, 100));
 		add(run, new GBC(0, 2).setAnchor(GBC.WEST).setWeight(100, 100));
 		add(days, new GBC(0, 3).setAnchor(GBC.WEST).setWeight(100, 100));
-		add(hourStart, new GBC(2, 3).setAnchor(GBC.WEST).setWeight(100, 100));
-		add(hourFinish, new GBC(2, 4).setAnchor(GBC.NORTHWEST).setWeight(100, 100));
+		add(hourStart, new GBC(2, 3).setAnchor(GBC.WEST).setWeight(0, 100));
+		add(hourFinish, new GBC(2, 4).setAnchor(GBC.NORTHWEST).setWeight(0, 200));
+		add(saveName, new GBC(0, 7).setAnchor(GBC.NORTHWEST).setWeight(100, 100));
 		add(fieldsPane, new GBC(1, 1, 3, 1).setAnchor(GBC.WEST).setWeight(100, 100));
-		add(daysPane, new GBC(1, 3, 1, 2).setAnchor(GBC.WEST).setWeight(100, 100));
+		add(daysPane, new GBC(1, 3, 1, 2 ).setAnchor(GBC.WEST).setWeight(100, 100));
 		add(symbolField, new GBC(1, 0, 3, 1).setAnchor(GBC.WEST).setWeight(100, 100));
 		add(hourBegField, new GBC(3, 3).setAnchor(GBC.WEST).setWeight(100, 100));
-		add(hourEndField, new GBC(3, 4).setAnchor(GBC.NORTHWEST).setWeight(100, 100));
+		add(hourEndField, new GBC(3, 4).setAnchor(GBC.NORTHWEST).setWeight(100, 200));
 		add(freqField, new GBC(1, 2).setAnchor(GBC.WEST).setWeight(100, 100));
-		add(hoursBox, new GBC(2, 2).setAnchor(GBC.WEST).setWeight(100, 100));
+		add(hoursBox, new GBC(2, 2, 2, 1).setAnchor(GBC.WEST).setWeight(100, 100));
 		add(log, new GBC(0, 5, 3, 1).setAnchor(GBC.NORTHWEST).setWeight(100, 100));
 		add(printToScreen, new GBC(0, 6, 3, 1).setAnchor(GBC.NORTHWEST).setWeight(100, 100));
-		add(subButton, new GBC(3, 7).setAnchor(GBC.NORTHWEST).setWeight(100, 100));
+		add(fileNameField, new GBC(1, 7, 3, 1).setAnchor(GBC.NORTHWEST).setWeight(100, 100));
+		add(subButton, new GBC(3, 8).setAnchor(GBC.NORTHEAST).setWeight(100, 100));
 	
 		add(buttonPane);
 	}
 	
-	public JSONObject something;
+	private JCheckBox log;
+	private JCheckBox printToScreen;
+	private JTextField freqField;
+	private JTextField symbolField; 
+	private JTextField hourBegField;
+	private JTextField hourEndField;
+	private JTextField fileNameField;
 	private JPanel buttonPane;
 	private JList fieldsList;
 	private JList daysList;
 	private JComboBox hoursBox;
 	
-	public static final int DEFAULT_WIDTH = 500;
-	public static final int DEFAULT_HEIGHT = 450;
+	public static final int DEFAULT_WIDTH = 425;
+	public static final int DEFAULT_HEIGHT = 470;
 }
 
